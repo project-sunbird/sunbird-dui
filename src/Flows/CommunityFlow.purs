cf690a73f71445f6c1626780ef0abd3a4008f8b9
kiranpuppala_juspay <kiran.puppala@juspay.in>
2017-06-27 17:47:04 +0530

    cleaned comments

--- src/Flows/CommunityFlow.purs
+++ src/Flows/CommunityFlow.purs
@@ -44,51 +44,6 @@ startCommunityViewAllFlow state = do
 
 
 
-  
-
-
-
-  --reqTokens <- getReqTokens
-  --responseData <- getUserCourses "user1"
-  --response <- getCourses reqTokens
-  --state <- updateState {response: responseData} state
-  --_ <- sendUpdatedState state
---   state <- getCallbackFromScreen "HOME" state
---   case state.action of
---     "showCommunityInfo" -> do
---       liftEff $ log "showCommunityInfoFlow"
---       showCommunityInfoFlow state
---       pure $ "showCommunityInfo handled"
---     "showAll" -> do
---       liftEff $ log "showAll"
---       showCommunityViewAllFlow state
---       pure $ "showCommunityViewAllFlow handled"
---     _ -> startCommunityFlow state
---          pure $ "action not handled"
-
--- showCommunityInfoFlow state = do
---   state <- showUI "COMMUNITY_INFO_SCREEN" state
---   case state.action of
---     "goBack" -> do
---       liftEff $ log "startCommunityFlow"
---       startCommunityFlow state
---       pure $ "startCommunityFlow handled"
---     _ ->showCommunityInfoFlow state
---         pure $ "action not handled"
-
--- showCommunityViewAllFlow state = do
---   state <- showUI "COMMUNITY_VIEW_ALL_SCREEN" state
---   case state.action of
---     "goBack" -> do
---       liftEff $ log "startCommunityFlow"
---       startCommunityFlow state
---       pure $ "startCommunityFlow handled"
---     _ ->showCommunityInfoFlow state
---         pure $ "action not handled"
-
-
-
-
 
 
 
