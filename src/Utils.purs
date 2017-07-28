c46f05cf20c0c66081790a51a4ff68ba7521c106
vinaynarayana_juspay <vinay.narayana@juspay.in>
2017-07-28 12:54:00 +0530

    course page api fix

--- src/Utils.purs
+++ src/Utils.purs
@@ -193,7 +193,7 @@ getCourcePageFilterApi user_token api_token filter_to_use=
       headers = (generateRequestHeaders user_token api_token)
       payload = A.fromObject (StrMap.fromFoldable [ (Tuple "id" (A.fromString "unique API ID"))
                                                    , (Tuple "ts" (A.fromString "2013/10/15 16:16:3"))
-                                                   , (Tuple "request" (A.fromObject (StrMap.fromFoldable  [ (Tuple "name" (A.fromString "Cource"))
+                                                   , (Tuple "request" (A.fromObject (StrMap.fromFoldable  [ (Tuple "name" (A.fromString "Course"))
                                                                                                           , (Tuple "source" (A.fromString "web"))
                                                                                                           , (Tuple "filters" (getJsonFromString filter_to_use))
                                                                                                                                                               
