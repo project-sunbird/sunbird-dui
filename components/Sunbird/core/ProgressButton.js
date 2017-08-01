e70b5c0f055a7880a856beffd10fe095c75e5bb1
vinaynarayana_juspay <vinay.narayana@juspay.in>
2017-08-01 17:26:17 +0530

    changed ntp staging to ntp prod

--- components/Sunbird/core/ProgressButton.js
+++ components/Sunbird/core/ProgressButton.js
@@ -195,7 +195,7 @@ class ProgressButton extends View {
 
         console.log("progress status", contentProgress)
           // JBridge.setInSharedPrefs(this.props.identifier, JSON.stringify(contentProgress));
-        var url = "https://staging.ntp.net.in/api/course/v1/content/state/update"
+        var url = "https://ntp.net.in/api/course/v1/content/state/update"
         
         console.log("date",date)
 
