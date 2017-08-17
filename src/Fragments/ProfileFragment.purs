8d3e6b115c65d7228c20401db8f4508c34177966
Nikith Shetty <nikith.shetty@juspay.in>
2017-08-17 17:15:42 +0530

    added Search user feature

--- src/Fragments/ProfileFragment.purs
+++ src/Fragments/ProfileFragment.purs
@@ -14,6 +14,7 @@ profileFragment input whereFrom whatToSendBack = do
 	event <- ui $ MainActivity
 	case event of
 		OPEN_NotificationActivity -> notificationActivity "{}" "ProfileFragment"  input
+		OPEN_CommProfSearchActivity {filterDetails : output} -> searchProfileActivity output "ProfileFragment" input
 		OPEN_EditProfileActivity -> additionalInformationActivity "{}" "profileFragment"  input
 		_ -> profileFragment input whereFrom whatToSendBack
 
@@ -25,6 +26,26 @@ notificationActivity input whereFrom whatToSendBack = do
 			_ -> profileFragment input "Terminate" input
 		_ -> notificationActivity input whereFrom whatToSendBack
 
+searchProfileActivity input whereFrom whatToSendBack = do
+	event <- ui $ CommProfSearchActivity {filterDetails:input}
+	case event of
+		API_SearchProfile {user_token:x, api_token:y, filter_to_send:z} -> do
+			resData <- searchUser x y z
+			_<- sendUpdatedState {response : resData, responseFor : "API_SearchProfile", screen:"asas"}
+			pure $ "apiCalled"
+		OPEN_ProfileActivity_SEARCH {profile: output} -> profileActivity output "CommProfSearchActivity" input
+		BACK_CommProfSearchActivity -> case whereFrom of
+			"ProfileFragment" -> profileFragment whatToSendBack "Terminate" input
+			_ -> profileFragment whatToSendBack "Terminate" input
+		_ -> searchProfileActivity input whereFrom whatToSendBack
+
+profileActivity input whereFrom whatToSendBack = do
+	event <- ui $ ProfileActivity {profile : input}
+	case event of
+		OPEN_CommProfSearchActivity_Prof {filterDetails : output} -> searchProfileActivity output "ProfileFragment" input
+		BACK_ProfileActivity -> searchProfileActivity whatToSendBack "Terminate" input
+		_ -> profileActivity input whereFrom whatToSendBack
+		
 additionalInformationActivity input whereFrom whatToSendBack = do
 	event <- ui $ AdditionalInformationActivity
 	case event of
