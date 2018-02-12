89ff1c24db471d2dec0dd64109ca23c89d489d03
Nikith Shetty <nikith.shetty@juspay.in>
2018-02-12 14:32:05 +0530

    Issue #SB-1586, #SB-1578 fix: username should not be displayed in guest mode

--- views/SplashScreenActivity.js
+++ views/SplashScreenActivity.js
@@ -20,26 +20,27 @@ class SplashScreenActivity extends View {
     this.getApiUrl();
     this.getIcon();
     this.getTextToDisplay();
-    window.__pressedLoggedOut=false;
+    window.__pressedLoggedOut = false;
+    window.__loggedInState = JBridge.getFromSharedPrefs("logged_in")
     this.isUserOnboarded = JBridge.getFromSharedPrefs("isUserOnboarded");
-  //  window.__getGenieEvents = this.dummyFunction;
+    //  window.__getGenieEvents = this.dummyFunction;
   }
 
   dummyFunction = (response) => {
-    console.log("response genie events",JSON.parse(utils.decodeBase64(response)));
+    console.log("response genie events", JSON.parse(utils.decodeBase64(response)));
   }
 
   getTextToDisplay = () => {
-      var textToDisplay = JBridge.getFromSharedPrefs("orgName");
-      if (textToDisplay == "__failed" || textToDisplay == ""){
-          textToDisplay = JBridge.getAppName();
-      }
-      this.textToDisplay = textToDisplay;
+    var textToDisplay = JBridge.getFromSharedPrefs("orgName");
+    if (textToDisplay == "__failed" || textToDisplay == "") {
+      textToDisplay = JBridge.getAppName();
+    }
+    this.textToDisplay = textToDisplay;
   }
 
   getIcon = () => {
     var icon = JBridge.getFromSharedPrefs("logo_url");
-    if (icon  == "__failed" || icon == "undefined" || icon == "") {
+    if (icon == "__failed" || icon == "undefined" || icon == "") {
       icon = "ic_launcher";
     }
     this.icon = icon;
@@ -52,37 +53,37 @@ class SplashScreenActivity extends View {
     );
   }
 
-  getApiUrl = ()=>{
+  getApiUrl = () => {
     var Url = JBridge.getApiUrl();
-    window.__loginUrl = "https://"+Url;
-    window.__apiUrl = "https://"+Url
+    window.__loginUrl = "https://" + Url;
+    window.__apiUrl = "https://" + Url
     window.__deepLinkUrl = Url;
   }
 
-  getUserToken = ()=>{
+  getUserToken = () => {
     window.__apiToken = JBridge.getFromSharedPrefs("api_token");
     console.log("in user token", window.__apiToken);
 
     if (window.__apiToken == "__failed" || window.__apiToken == "") {
-        JBridge.getApiToken(callbackMapper.map(token => {
-            console.log("user token", token[0]);
-            window.__apiToken = token[0];
-            JBridge.setInSharedPrefs("api_token", token[0]);
-        }));
+      JBridge.getApiToken(callbackMapper.map(token => {
+        console.log("user token", token[0]);
+        window.__apiToken = token[0];
+        JBridge.setInSharedPrefs("api_token", token[0]);
+      }));
     }
   }
 
   afterRender = () => {
 
     var epoch = JBridge.epochTime();
-      var connectionType = JBridge.checkConnectionType();
-      if(epoch < 26979 || connectionType == "wifi")
-        JBridge.syncTelemetry(15000); // 15 seconds delay if its first day of installation or device is connected to wifi
-      else
-        JBridge.syncTelemetry(30000); // 30 seconds delay otherwise
+    var connectionType = JBridge.checkConnectionType();
+    if (epoch < 26979 || connectionType == "wifi")
+      JBridge.syncTelemetry(15000); // 15 seconds delay if its first day of installation or device is connected to wifi
+    else
+      JBridge.syncTelemetry(30000); // 30 seconds delay otherwise
 
     JBridge.logsplashScreenEvent();
-    JBridge.logCorrelationPageEvent("SPLASHSCREEN","","");
+    JBridge.logCorrelationPageEvent("SPLASHSCREEN", "", "");
 
     setTimeout(() => {
       if (window.__loggedInState == "YES" || window.__loggedInState == "GUEST") {
@@ -109,17 +110,17 @@ class SplashScreenActivity extends View {
         background={window.__Colors.WHITE}
         orientation="vertical">
 
-          <ImageView
-            height="250"
-            width="250"
-            layout_gravity="center"
-            circularImageUrl = {"1," + this.icon}/>
-          <TextView
-            text={this.textToDisplay}
-            margin="20,120,20,20"
-            layout_gravity="center"
-            textSize = "18"
-            height="wrap_content"/>
+        <ImageView
+          height="250"
+          width="250"
+          layout_gravity="center"
+          circularImageUrl={"1," + this.icon} />
+        <TextView
+          text={this.textToDisplay}
+          margin="20,120,20,20"
+          layout_gravity="center"
+          textSize="18"
+          height="wrap_content" />
       </LinearLayout>
     );
 
