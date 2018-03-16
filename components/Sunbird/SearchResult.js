ab281cfdd7d3f159f3fa2fb0903085eb41aa4d41
Nikhil Roy <nikhil.roy@juspay.in>
2018-03-16 13:14:47 +0530

    Issue #SB-2223: search result ui changes

--- components/Sunbird/SearchResult.js
+++ components/Sunbird/SearchResult.js
@@ -20,9 +20,11 @@ class SearchResult extends View {
     this.type = this.props.type ? this.props.type : "Resource";
     this.jsonArray = [];
     this.setIds([
-      'listContainer'
+      'listContainer',
+      "gradeTextView"
     ]);
     this.handleItemClick = debounce(this.handleItemClick, 50);
+    this.screenWidth = JBridge.getScreenWidth();
   }
   getData = () => {
     return (<ListView
@@ -33,6 +35,7 @@ class SearchResult extends View {
   }
   showList = () => {
     this.props.data.map((item, index) => {
+      var halfWidth = Math.floor(((this.screenWidth-100)/2));
       var appIcon = "ic_launcher";
       if (this.type == "Profile") {
         appIcon = (item.data && item.data.avatar) ? item.data.avatar : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR1X3cm5xzR4D1W9oPb2QWioKlrfLVd0DvXFUNqSjZfg-M0bpc";
@@ -52,9 +55,9 @@ class SearchResult extends View {
             height="wrap_content">
 
             <ImageView
-              width="32"
-              height="32"
-              margin="10,12,0,12"
+              width="37"
+              height="37"
+              margin={item.subject || item.gradeLevel? "10,22,0,22":"10,12,0,12"}
               scaleType="fixXY"
               gravity="center"
               circularImageUrl={"0," + appIcon} />
@@ -70,35 +73,111 @@ class SearchResult extends View {
 
                 <TextView
                   height="wrap_content"
-                  width="0"
+                  width="wrap_content"
                   padding="10,10,0,0"
                   enableEllipse="true"
                   text={item.name}
-                  weight="7"
+                  weight="0"
                   style={window.__TextStyle.textStyle.CARD.HEADING} />
 
-                <LinearLayout
-                  width="0"
-                  weight="0.5" />
-
-                <TextView
-                  height="wrap_content"
-                  padding="0,10,10,0"
-                  weight="2"
-                  gravity="right"
-                  width="0"
-                  visibility={item.hasOwnProperty("size") ? "visible" : "gone"}
-                  text={item.hasOwnProperty("size") ? utils.formatBytes(item.size) : " "}
-                  style={window.__TextStyle.textStyle.HINT.SEMI} />
+                  <ImageView
+                    width="16"
+                    height="16"
+                    gravity="left"
+                    margin="4,10,0,0"
+                    imageUrl="ic_check_circle"/>
               </LinearLayout>
-
+              <LinearLayout
+                orientation="horizontal">
               <TextView
                 height="wrap_content"
-                padding="10,3,10,10"
+                padding="10,4,0,0"
                 width="wrap_content"
                 visibility={item.contentType ? "visible" : "gone"}
                 text={item.contentType}
                 style={window.__TextStyle.textStyle.HINT.SEMI} />
+
+                <ImageView
+                  width="10"
+                  height="10"
+                  gravity="left"
+                  margin="4,7,0,0"
+                  imageUrl="ic_dot_lightgrey"/>
+
+                  <ImageView
+                    width="12"
+                    height="12"
+                    gravity="left"
+                    margin="4,6,0,0"
+                    imageUrl="ic_grey_star"/>
+
+                    <TextView
+                       height="wrap_content"
+                       padding="4,4,0,0"
+                       gravity="left"
+                       width="wrap_content"
+                       text={"4.0"}
+                       style={window.__TextStyle.textStyle.HINT.SEMI} />
+
+
+                  <ImageView
+                    width="10"
+                    height="10"
+                    gravity="left"
+                    margin="4,7,0,0"
+                    imageUrl="ic_dot_lightgrey"/>
+
+                    <TextView
+                       height="wrap_content"
+                       padding="4,4,0,0"
+                       gravity="left"
+                       width="wrap_content"
+                       visibility={item.hasOwnProperty("size") ? "visible" : "gone"}
+                       text={item.hasOwnProperty("size") ? utils.formatBytes(item.size) : " "}
+                       style={window.__TextStyle.textStyle.HINT.SEMI} />
+                   </LinearLayout>
+
+                   <LinearLayout
+                      orientation="horizontal"
+                      padding = "10,0,0,0">
+
+                      <TextView
+                         height="wrap_content"
+                         id={this.idSet.gradeTextView}
+                         width={item.gradeLevel && item.gradeLevel.length > 2 && item.subject ? halfWidth+"" : "wrap_content"}
+                         padding={item.subject ? "0,4,0,10":"0,4,16,10"}
+                         gravity="left"
+                         enableEllipse="true"
+                         singleLine="true"
+                         scrollHorizontally="true"
+                         visibility={item.gradeLevel?"visible":"gone"}
+                         text={item.gradeLevel ? item.gradeLevel.toString().replace(/,/g , ", ") : ""}
+                         style={window.__TextStyle.textStyle.HINT.SEMI} />
+
+
+                         <ImageView
+                           width="10"
+                           height="10"
+                           visibility={item.gradeLevel && item.subject ? "visible":"gone"}
+                           gravity="left"
+                           margin="4,7,0,0"
+                           imageUrl="ic_dot_lightgrey"/>
+
+                           <TextView
+                              height="wrap_content"
+                              padding="4,3,16,10"
+                              gravity="left"
+                              width="wrap_content"
+                              enableEllipse="true"
+                              singleLine="true"
+                              visibility={item.subject?"visible":"gone"}
+                              text={item.subject ? item.subject.toString() : ""}
+                              style={window.__TextStyle.textStyle.HINT.SEMI} />
+
+
+
+                      </LinearLayout>
+
 
               <LinearLayout
                 width="wrap_content"
